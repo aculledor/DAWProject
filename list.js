992c9dfdeaaf6ad64f037f658efbd78548743c86
Abraham Trashorras <abraham.cocodin@gmail.com>
2020-11-15 18:47:44 +0100

    p6

--- list.js
+++ list.js
@@ -3,3 +3,19 @@
 function showSubmenu() {
     document.getElementById("subMenu").classList.toggle("hide");
 }
+
+var xhttp =  new XMLHttpRequest();
+xhttp.onreadystatechange =  function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var xmlDoc= this.responseXML;
+        var txt = "<ul>";
+        var data = xmlDoc.getElementsByTagName("NAME");
+        for(var i = 0; i < data.length; i++){
+            txt = txt + "<li>" + data[i].childNodes[0].nodeValue + "</li>";
+        }
+        var txt = txt + "<ul>";
+        document.getElementById("items").innerHTML = txt;
+    }
+  };
+xhttp.open("GET", "http://127.0.0.1:80/itemList.xml", true);
+xhttp.send();
